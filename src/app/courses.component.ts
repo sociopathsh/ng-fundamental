b50806f3cf708a565d350daccda9ec58560ff343
Sherlock <sherlock@gmail.com>
2018-04-23 16:13:09 +0630

    Data binding in template

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     'selector': 'courses',
-    'template': '<h2>Courses</h2>'
+    'template': '<h2>{{ title }}</h2><h3>{{ getTitle() }}</h3>'
 })
 
 export class CoursesComponent {
-
+    title = "List of courses";
+    getTitle() {
+        return this.title;
+    }
 }
