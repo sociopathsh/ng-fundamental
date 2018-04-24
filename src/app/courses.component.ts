9a59039826850890b4b92f5ec052bf27313bcb09
Sherlock <sherlock@gmail.com>
2018-04-24 13:51:03 +0630

    Class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,10 +4,10 @@ import {CoursesService} from "./courses.service";
 @Component({
     'selector': 'courses',
     'template': `
-        <button class="btn btn-primary">Button</button>
+        <button class="btn btn-primary" [class.active]="isActive" [class.disabled]="isActive">Button</button>
         `
 })
 
 export class CoursesComponent {
-
+    isActive = false;
 }
